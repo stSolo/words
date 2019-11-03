c180b7b38bf2788f57bfc6383fdd86ab2a362104
StSolo <StSolo>
2019-11-04 02:27:56 +0300

    Updates

---
+++ precache-manifest.c56218ff2110feed61f626ac9b6d9b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027b265e3422344dc2fb24f0023e0b5a",
+    "url": "/words/index.html"
+  },
+  {
+    "revision": "e04c2cd1a97f96e62f7e",
+    "url": "/words/static/css/main.ac8d1546.chunk.css"
+  },
+  {
+    "revision": "13c66014a6f8e8710db9",
+    "url": "/words/static/js/2.42ec9c83.chunk.js"
+  },
+  {
+    "revision": "e04c2cd1a97f96e62f7e",
+    "url": "/words/static/js/main.86266286.chunk.js"
+  },
+  {
+    "revision": "b310aaf1668d880d92a4",
+    "url": "/words/static/js/runtime-main.edcb6ac9.js"
+  }
+]);
